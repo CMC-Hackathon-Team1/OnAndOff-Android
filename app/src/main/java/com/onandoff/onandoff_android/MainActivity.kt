c5a7fe10e9ed98671cd02db71eb66b3999029c2c
thsamajiki <ant1102@naver.com>
2023-01-15 16:42:52 +0900

    MainActivity 클래스와 레이아웃 생성, BottomNavigation 추가 및 리스너 추가

--- app/src/main/java/com/onandoff/onandoff_android/MainActivity.kt
+++ app/src/main/java/com/onandoff/onandoff_android/MainActivity.kt
@@ -2,10 +2,60 @@ package com.onandoff.onandoff_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import com.google.android.material.navigation.NavigationBarView
+import com.onandoff.onandoff_android.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener  {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+
+        setupFragmentAdapter()
+        setupBottomNavigationView()
+
+        if (savedInstanceState == null) {
+            binding.bottomNavMain.selectedItemId = R.id.menu_home
+        }
+    }
+
+    private fun setupFragmentAdapter() {
+        val fragmentAdapter = FragmentAdapter(this)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.commit()
+//        fragmentTransaction.add(binding.mainFcv.id, recordFragment, binding.mainFcv.tag)
+
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavMain.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        when(item.itemId) {
+            R.id.menu_home -> {
+                fragmentTransaction.replace(binding.fcvMain.id, HomeFragment.newInstance()).commit()
+            }
+            R.id.menu_look_around -> {
+            }
+            R.id.menu_my_page -> {
+
+            }
+            else -> {
+                throw IllegalArgumentException("Not found menu item")
+            }
+        }
+
+        return true
     }
 }
