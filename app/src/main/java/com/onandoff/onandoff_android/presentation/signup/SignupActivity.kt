a8399c9e6764e1bf4e5c7c6b2353e6464b8fb4be
kimwest00 <kimwest00@hufs.ac.kr>
2023-01-21 16:49:05 +0900

    feat -signup 페이지 레이아웃

---
+++ app/src/main/java/com/onandoff/onandoff_android/presentation/signup/SignupActivity.kt
@@ -0,0 +1,14 @@
+package com.onandoff.onandoff_android.presentation.signup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.onandoff.onandoff_android.databinding.ActivitySignupBinding
+
+class SignupActivity:AppCompatActivity() {
+    private lateinit var binding : ActivitySignupBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
