eb14ba167e356347f6170dbe092cd3aa0db02dba
thsamajiki <ant1102@naver.com>
2023-01-26 12:33:04 +0900

    데이터 클래스 이름 변경

--- app/src/main/java/com/onandoff/onandoff_android/presentation/home/MyProfileListAdapter.kt
+++ app/src/main/java/com/onandoff/onandoff_android/presentation/home/MyProfileListAdapter.kt
@@ -7,28 +7,28 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.onandoff.onandoff_android.data.model.MyPersona
+import com.onandoff.onandoff_android.data.model.MyPersonaData
 import com.onandoff.onandoff_android.databinding.ItemMyProfileBinding
 
 class MyProfileListAdapter(
-    private val onClick: (MyPersona) -> Unit
-): ListAdapter<MyPersona, MyProfileListAdapter.MyProfileViewHolder>(MyProfileDiffUtil) {
+    private val onClick: (MyPersonaData) -> Unit
+): ListAdapter<MyPersonaData, MyProfileListAdapter.MyProfileViewHolder>(MyProfileDiffUtil) {
 
     class MyProfileViewHolder(
         private val binding: ItemMyProfileBinding,
-        private val onClick: (MyPersona) -> Unit
+        private val onClick: (MyPersonaData) -> Unit
     ): RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(myPersona: MyPersona, position: Int) {
-            binding.tvMyProfile.text = myPersona.name
+        fun bind(myPersonaData: MyPersonaData, position: Int) {
+            binding.tvMyProfile.text = myPersonaData.name
             binding.viewLeft.isVisible = position == 0
 
             Glide.with(binding.root.context)
-                .load(myPersona.profileImageUrl)
+                .load(myPersonaData.profileImageUrl)
                 .into(binding.ivMyProfile)
 
             binding.root.setOnClickListener {
-                onClick(myPersona)
+                onClick(myPersonaData)
             }
         }
     }
@@ -43,12 +43,12 @@ class MyProfileListAdapter(
         holder.bind(getItem(position), position)
     }
 
-    companion object MyProfileDiffUtil : DiffUtil.ItemCallback<MyPersona>() {
-        override fun areItemsTheSame(oldItem: MyPersona, newItem: MyPersona): Boolean {
+    companion object MyProfileDiffUtil : DiffUtil.ItemCallback<MyPersonaData>() {
+        override fun areItemsTheSame(oldItem: MyPersonaData, newItem: MyPersonaData): Boolean {
             return oldItem.name == newItem.name
         }
 
-        override fun areContentsTheSame(oldItem: MyPersona, newItem: MyPersona): Boolean {
+        override fun areContentsTheSame(oldItem: MyPersonaData, newItem: MyPersonaData): Boolean {
             return oldItem == newItem
         }
     }
--- app/src/main/java/com/onandoff/onandoff_android/presentation/look/LookAroundFragment.kt
+++ app/src/main/java/com/onandoff/onandoff_android/presentation/look/LookAroundFragment.kt
@@ -12,7 +12,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.tabs.TabLayout
-import com.onandoff.onandoff_android.data.model.LookAround
+import com.onandoff.onandoff_android.data.model.LookAroundData
 import com.onandoff.onandoff_android.R
 import com.onandoff.onandoff_android.databinding.FragmentLookAroundBinding
 
@@ -22,8 +22,8 @@ class LookAroundFragment : Fragment() {
         get() = _binding!!
 
     private lateinit var lookAroundListAdapter: LookAroundListAdapter
-    private val searchLookAroundList = mutableListOf<LookAround>()
-    private val followingLookAroundList = mutableListOf<LookAround>()
+    private val searchLookAroundDataList = mutableListOf<LookAroundData>()
+    private val followingLookAroundDataList = mutableListOf<LookAroundData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -56,11 +56,11 @@ class LookAroundFragment : Fragment() {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 when (tab.position) {
                     0 -> {
-                        lookAroundListAdapter.setLookAroundListOnTab(searchLookAroundList)
+                        lookAroundListAdapter.setLookAroundListOnTab(searchLookAroundDataList)
                         lookAroundListAdapter.notifyDataSetChanged()
                     }
                     1 -> {
-                        lookAroundListAdapter.setLookAroundListOnTab(followingLookAroundList)
+                        lookAroundListAdapter.setLookAroundListOnTab(followingLookAroundDataList)
                         lookAroundListAdapter.notifyDataSetChanged()
                     }
                 }
@@ -188,17 +188,17 @@ class LookAroundFragment : Fragment() {
         }
     }
 
-    private fun intentPost(lookAround: LookAround) {
+    private fun intentPost(lookAroundData: LookAroundData) {
         // TODO: 해당 데이터의 상세 페이지로 이동하기
 //        val intent = UserPostActivity.getIntent(requireActivity(), lookAroundData.key)
 //        startActivity(intent)
     }
 
-    private fun addToFollowingList(lookAround: LookAround) {
+    private fun addToFollowingList(lookAroundData: LookAroundData) {
         // TODO: 해당 데이터를 팔로잉 목록에 추가하기
     }
 
-    private fun addLike(lookAround: LookAround) {
+    private fun addLike(lookAroundData: LookAroundData) {
         // TODO: 해당 데이터에 좋아요 개수를 +1 하기
     }
 
