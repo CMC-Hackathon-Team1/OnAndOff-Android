731858a71640bde34e7ab4f22076835b80db1da9
kimwest00 <kimwest00@hufs.ac.kr>
2023-01-30 02:47:20 +0900

    layout - splash 화면 레이아웃 수정

--- app/src/main/java/com/onandoff/onandoff_android/data/api/util/API_CONSTANTS.kt
+++ app/src/main/java/com/onandoff/onandoff_android/data/api/util/API_CONSTANTS.kt
@@ -2,5 +2,5 @@ package com.onandoff.onandoff_android.data.api.util
 
 //서버 관련 정보를 선언한 변수들
 object API_CONSTANTS {
-    const val BASE_URL :String = "http://43.201.136.61:5050/"
+    const val BASE_URL :String = "https://dev.onnoff.shop/"
 }
--- app/src/main/java/com/onandoff/onandoff_android/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/onandoff/onandoff_android/presentation/splash/SplashActivity.kt
@@ -24,13 +24,13 @@ class SplashActivity:AppCompatActivity() {
 //        Log.d("Hash", keyHash)
         var userEmail = prefs.getSharedPreference("email", "")
         Log.d("splash", userEmail)
-        //1번째는 데이터 키 값이고 2번째는 키 값에 데이터가 존재하지않을때 대체 값 입니다.
-//        if(userEmail != ""){
-//            val intent = Intent(applicationContext, MainActivity::class.java)
-//            startActivity(intent)
-//            Toast.makeText(this, "로그인 되었습니다", Toast.LENGTH_SHORT).show()
-//            finish()
-//        }
+
+        if(userEmail != ""){
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "로그인 되었습니다", Toast.LENGTH_SHORT).show()
+            finish()
+        }
         binding.btServiceLogin.setOnClickListener{
             val Intent = Intent(this, SignInActivity::class.java)
             startActivity(Intent)
