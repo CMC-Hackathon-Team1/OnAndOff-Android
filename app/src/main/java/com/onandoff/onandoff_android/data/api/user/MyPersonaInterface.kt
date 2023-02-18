43443f1a67199369ee4f04c11c954bede2d0c325
thsamajiki <ant1102@naver.com>
2023-02-18 13:14:49 +0900

    페르소나 생성, 불러오기 API 인터페이스 추가

---
+++ app/src/main/java/com/onandoff/onandoff_android/data/api/user/MyPersonaInterface.kt
@@ -0,0 +1,29 @@
+package com.onandoff.onandoff_android.data.api.user
+
+import com.onandoff.onandoff_android.data.model.CreateMyProfileResponse
+import com.onandoff.onandoff_android.data.model.MyProfileListResponse
+import com.onandoff.onandoff_android.data.model.MyProfileResponse
+import okhttp3.MultipartBody
+import retrofit2.http.*
+
+interface MyPersonaInterface {
+
+    @Multipart
+    @POST("/profiles")
+    suspend fun createMyPersona(
+        @Part profileName: MultipartBody.Part,
+        @Part personaName: MultipartBody.Part,
+        @Part statusMessage: MultipartBody.Part,
+        @Part image: MultipartBody.Part
+    ): CreateMyProfileResponse
+
+    @Multipart
+    @GET("/profiles/my-profiles/{}")
+    suspend fun getMyPersona(
+        @Path("profileId") profileId: Int
+    ): MyProfileResponse
+
+    @GET("/profiles/my-profiles")
+    suspend fun getMyPersonaList(): MyProfileListResponse
+}
+
