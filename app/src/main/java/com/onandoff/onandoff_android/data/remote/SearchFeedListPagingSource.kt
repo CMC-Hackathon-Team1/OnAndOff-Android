ef011c1aa8f9be761ff8a82e8c424045bad0eb1c
thsamajiki <ant1102@naver.com>
2023-03-03 16:27:48 +0900

    해시태그 검색에 필요한 데이터 수정 및 페이징 라이브러리 적용

---
+++ app/src/main/java/com/onandoff/onandoff_android/data/remote/SearchFeedListPagingSource.kt
@@ -0,0 +1,56 @@
+package com.onandoff.onandoff_android.data.remote
+
+import android.util.Log
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.onandoff.onandoff_android.data.model.FeedResponse
+import com.onandoff.onandoff_android.data.repository.FeedRequest
+
+class SearchFeedListPagingSource(
+    private val feedRemoteDataSource: FeedRemoteDataSource,
+    private val request: FeedRequest
+) : PagingSource<Int, FeedResponse>() {
+    init {
+        Log.d("feedRequest", "${hashCode()}")
+    }
+    override fun getRefreshKey(state: PagingState<Int, FeedResponse>): Int {
+        return INITIAL_KEY
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, FeedResponse> {
+        val page = params.key ?: 1
+        Log.d("feedRequest page", "$page")
+
+        val feedResponse =
+            kotlin.runCatching {
+                feedRemoteDataSource.getSearchFeedResult(
+                    profileId = request.profileId,
+                    page = page,
+                    categoryId = request.categoryId,
+                    fResult = request.fResult,
+                    query = request.query
+                )
+            }
+                .onFailure {
+                    return LoadResult.Error(it)
+                }
+
+        val list = feedResponse.getOrNull()?.result.orEmpty()
+
+        return LoadResult.Page(
+            data = list,
+            prevKey = null,
+            nextKey = if (list.isNotEmpty()) page + 1 else null
+        )
+
+        // 리스트가 비어 있는가?
+        // 1페이지 2개. 2페이지 요청 그냥 해봄. 2페이지 0개. 3페이지 요청 안함.
+
+        // 받은 리스트가 10개 미만인가?
+        // 1페이지 2개. 2페이지 요청 안함.
+    }
+
+    companion object {
+        const val INITIAL_KEY = 1
+    }
+}
