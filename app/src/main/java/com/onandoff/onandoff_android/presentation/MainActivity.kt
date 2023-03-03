94772c21cd63bbeb485ec4d95c9f736c71d34ae9
thsamajiki <ant1102@naver.com>
2023-03-03 16:29:14 +0900

    프래그먼트 간 이동해도 데이터 유지되도록 수정함

--- app/src/main/java/com/onandoff/onandoff_android/presentation/MainActivity.kt
+++ app/src/main/java/com/onandoff/onandoff_android/presentation/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.onandoff.onandoff_android.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.navigation.NavigationBarView
 import com.onandoff.onandoff_android.FragmentAdapter
 import com.onandoff.onandoff_android.R
 import com.onandoff.onandoff_android.databinding.ActivityMainBinding
 import com.onandoff.onandoff_android.presentation.home.HomeFragment
+import com.onandoff.onandoff_android.presentation.look.FeedListFragment
 import com.onandoff.onandoff_android.presentation.mypage.MypageFragment
-import com.onandoff.onandoff_android.presentation.look.LookAroundFragment
 
-class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener  {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,17 +46,45 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val prevFragment = supportFragmentManager.fragments.find {
+            it.isVisible
+        }
 
-        when(item.itemId) {
+        if (prevFragment != null) {
+            supportFragmentManager.beginTransaction().hide(prevFragment).commitNow()
+        }
+
+        when (item.itemId) {
             R.id.menu_home -> {
-                fragmentTransaction.replace(binding.fcvMain.id, HomeFragment.newInstance()).commit()
+                val homeFragment = supportFragmentManager.fragments.find { it is HomeFragment }
+                if (homeFragment != null) {
+                    supportFragmentManager.beginTransaction().show(homeFragment).commit()
+                } else {
+                    supportFragmentManager.beginTransaction()
+                        .add(binding.fcvMain.id, HomeFragment.newInstance())
+                        .commit()
+                }
             }
             R.id.menu_look_around -> {
-                fragmentTransaction.replace(binding.fcvMain.id, LookAroundFragment.newInstance()).commit()
+                val feedListFragment =
+                    supportFragmentManager.fragments.find { it is FeedListFragment }
+                if (feedListFragment != null) {
+                    supportFragmentManager.beginTransaction().show(feedListFragment).commit()
+                } else {
+                    supportFragmentManager.beginTransaction().add(
+                        binding.fcvMain.id,
+                        FeedListFragment.newInstance()
+                    ).commit()
+                }
             }
             R.id.menu_my_page -> {
-                fragmentTransaction.replace(binding.fcvMain.id, MypageFragment()).commit()
+                val myPageFragment = supportFragmentManager.fragments.find { it is MypageFragment }
+                if (myPageFragment != null) {
+                    supportFragmentManager.beginTransaction().show(myPageFragment).commit()
+                } else {
+                    supportFragmentManager.beginTransaction()
+                        .add(binding.fcvMain.id, MypageFragment()).commit()
+                }
             }
             else -> {
                 throw IllegalArgumentException("Not found menu item")
@@ -64,4 +93,6 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
 
         return true
     }
+
+
 }
