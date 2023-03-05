9a8b6da5dbfd32505c6f4e1086f27c07cfe14b02
thsamajiki <ant1102@naver.com>
2023-03-06 01:48:40 +0900

    텍스트 바인딩 어댑터 추가

--- app/src/main/java/com/onandoff/onandoff_android/data/ext/TextBindingAdapterExt.kt
+++ app/src/main/java/com/onandoff/onandoff_android/data/ext/TextBindingAdapterExt.kt
@@ -1,19 +1,17 @@
 package com.onandoff.onandoff_android.data.ext
 
 import android.widget.TextView
+import androidx.core.view.isGone
 import androidx.databinding.BindingAdapter
+import com.onandoff.onandoff_android.R
 import java.text.SimpleDateFormat
 
-@BindingAdapter("timestamp")
-fun TextView.setTimestamp(timestamp: String?) {
-    timestamp ?: return
-
-    val simpleDateFormat = SimpleDateFormat("yyyy년 MM월 dd일")
-//    val simpleDateFormat = SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분")
-    text = simpleDateFormat.format(timestamp.toString())
+@BindingAdapter("joinToTextList")
+fun TextView.setJoinToTextList(textList: List<String>) {
+    if (textList.isEmpty()) {
+        this.text = ""
+        this.isGone = true
+    } else {
+        this.text = textList.joinToString(" #", "#")
+    }
 }
-
-
-
-fun Long.convertDate(): String =
-    SimpleDateFormat("yyyy년 MM월 dd일").toString()
