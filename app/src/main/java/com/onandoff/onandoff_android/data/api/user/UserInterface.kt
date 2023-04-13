b448cdd0d9a701049f962aea71ded8b85f1fd96a
thsamajiki <ant1102@naver.com>
2023-04-13 11:50:25 +0900

    차단/차단 해제 및 차단된 유저 목록 조회 API 1차 구현

--- app/src/main/java/com/onandoff/onandoff_android/data/api/user/UserInterface.kt
+++ app/src/main/java/com/onandoff/onandoff_android/data/api/user/UserInterface.kt
@@ -1,8 +1,8 @@
 package com.onandoff.onandoff_android.data.api.user
 
 import com.google.gson.JsonElement
-import com.google.gson.annotations.SerializedName
 import com.onandoff.onandoff_android.data.model.*
+import com.onandoff.onandoff_android.data.request.BlockOrUnblockOtherUserRequest
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -35,5 +35,13 @@ interface UserInterface {
     @GET("/users/email")
     fun getEmail():Call<getMyEmail>
 
+    @POST("/profile-block")
+    suspend fun blockOrUnblockOtherUser(
+        @Body blockOrUnblockOtherUserRequest: BlockOrUnblockOtherUserRequest
+    ): BlockOtherUserResponse
 
+    @GET("/profile-block/blocked-profiles")
+    suspend fun getBlockedUserList(
+        @Path("profileId") profileId: Int
+    ) : GetBlockedUserListResponse
 }
