90e855d3f45860f6e49727214c1d733c4fb226ea
noion0511 <noion0511@gmail.com>
2023-04-30 20:06:03 +0900

    feat : token이 바뀌면 업데이트하는 기능

--- app/src/main/java/com/onandoff/onandoff_android/presentation/MainActivity.kt
+++ app/src/main/java/com/onandoff/onandoff_android/presentation/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.onandoff.onandoff_android.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.messaging.FirebaseMessaging
@@ -60,30 +63,70 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
 
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
             if (!task.isSuccessful) {
+                Log.w(TAG, "FCM 토큰 얻기에 실패하였습니다.", task.exception)
                 return@OnCompleteListener
             }
+            // token log 남기기
+            Log.d(TAG, "token: ${task.result?:"task.result is null"}")
+            if(task.result != null){
+                uploadToken(task.result!!)
 
-            val token = task.result
-            val tokenData = TokenData(token)
-            notificationInterface?.setAlarmTokenResponse(tokenData)?.enqueue(object :
-                Callback<NotificationResponse> {
-                override fun onResponse(
-                    call: Call<NotificationResponse>,
-                    response: Response<NotificationResponse>
-                ) {
-                    if (response.code() == 201){
-                        Log.d(TAG, "onResponse: Fcm token setting complete")
+                val token = task.result
+                val tokenData = TokenData(token)
+
+                notificationInterface?.setAlarmTokenResponse(tokenData)?.enqueue(object :
+                    Callback<NotificationResponse> {
+                    override fun onResponse(
+                        call: Call<NotificationResponse>,
+                        response: Response<NotificationResponse>
+                    ) {
+                        if (response.code() == 201){
+                            Log.d(TAG, "onResponse: Fcm token setting complete")
+                        }
+                    }
+
+                    override fun onFailure(call: Call<NotificationResponse>, t: Throwable) {
+                        TODO("Not yet implemented")
+                    }
+                })
+            }
+        })
+
+        createNotificationChannel(channel_id, "notice")
+    }
+
+
+    private fun createNotificationChannel(id: String, name: String) {
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(id, name, importance)
+
+        val notificationManager: NotificationManager
+                = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object{
+        // Notification Channel ID
+        const val channel_id = "notification_channel"
+        fun uploadToken(token:String){
+            // 새로운 토큰 수신 시 서버로 전송
+            val data = TokenData(token)
+            val storeService = RetrofitClient.getClient()?.create(NotificationInterface::class.java)
+            storeService?.setAlarmTokenResponse(data)?.enqueue(object : Callback<NotificationResponse> {
+                override fun onResponse(call: Call<NotificationResponse>, response: Response<NotificationResponse>) {
+                    if(response.isSuccessful){
+                        val res = response.body()
+                        Log.d(TAG, "onResponse: $res")
+                    } else {
+                        Log.d(TAG, "onResponse: Error Code ${response.code()}")
                     }
                 }
-
                 override fun onFailure(call: Call<NotificationResponse>, t: Throwable) {
-                    TODO("Not yet implemented")
+                    Log.d(TAG, t.message ?: "토큰 정보 등록 중 통신오류")
                 }
-
             })
-
-
-        })
+        }
     }
 
     private fun setupFragmentAdapter() {
