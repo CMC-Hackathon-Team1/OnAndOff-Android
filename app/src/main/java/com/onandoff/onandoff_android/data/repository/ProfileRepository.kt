a29fede77926087d7f7cd9b3dc1671b632ce3af8
thsamajiki <ant1102@naver.com>
2023-06-18 21:57:12 +0900

    MyProfileResponse 를 MyProfileItem 으로 변경하고 불필요한 메소드 제거함

--- app/src/main/java/com/onandoff/onandoff_android/data/repository/ProfileRepository.kt
+++ app/src/main/java/com/onandoff/onandoff_android/data/repository/ProfileRepository.kt
@@ -4,9 +4,10 @@ import com.onandoff.onandoff_android.data.model.CreateMyProfileResponse
 import com.onandoff.onandoff_android.data.model.MyProfileListResponse
 import com.onandoff.onandoff_android.data.model.MyProfileResponse
 import com.onandoff.onandoff_android.data.request.CreateProfileRequest
+import com.onandoff.onandoff_android.presentation.home.viewmodel.MyProfileItem
 
 interface ProfileRepository {
-    suspend fun getMyProfile(profileId: Int): MyProfileResponse
+    suspend fun getMyProfile(profileId: Int): MyProfileItem
 
     suspend fun getMyProfileList(): MyProfileListResponse
 
